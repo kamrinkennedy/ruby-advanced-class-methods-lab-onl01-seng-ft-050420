f9d615800497a31bfe2e54e88ff431c14997dbc0
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-14 18:56:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,34 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
+  def self.create
+    song = new
+    Song.all << song
+    song
+  end
+  
+  def self.new_by_name(title)
+    song = new 
+    song.name = title
+    song
+  end
+  
+  def self.create_by_name(title)
+    song = new 
+    song.name = title
+    Song.all << song
+    song
+  end
+  
+  def self.find_by_name(title)
+    Song.all.each do |song|
+      if song.name == title
+        return song
+      end
+    end
+  end
+  
   def self.all
     @@all
   end
